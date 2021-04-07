d7dac77840545f2defeac1d06be1fb3875e38e47
Heather McMillan <heather.o.mcmillan@gmail.com>
2021-04-06 23:40:15 -0500

    update with more work on data types exercises

--- data_types_and_variables.py
+++ data_types_and_variables.py
@@ -13,6 +13,42 @@ movies = [mermaid, bear, hercules]
 cost = 3 
 cost_per_movie = [movie * cost for movie in movies]
 total_cost = sum(cost_per_movie)
-print (total_cost)
+print ("total cost is:", total_cost)
+
+
+# Suppose you're working as a contractor for 3 companies: 
+# Google, Amazon and Facebook, they pay you a different rate per hour 
+# Google pays 400 dollars per hour 
+# Amazon 380, and Facebook 350 
+# How much will you receive in payment for this week? 
+# You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
+
+google = 400
+amazon = 380
+facebook = 350
+
+paycheck = 10 * facebook + 6 * google + 4 * amazon
+print("my paycheck will be ", paycheck, " dollars") 
+
+# A student can be enrolled to a class only if: 
+# the class is not full 
+# and the class schedule does not conflict with her current schedule
+
+class_full = False
+conflicts_with_schedule = False
+
+can_be_enrolled = not class_full and not conflicts_with_schedule
+print(can_be_enrolled)
+
+# A product offer can be applied only if people buys more than 2 items 
+# and the offer has not expired 
+# Premium members do not need to buy a specific amount of products
+
+number_of_items_bought = 3
+offer_expired = True
+is_premium_member = True
+
+eligible_for_product_offer =  not offer_expired and number_of_items_bought > 2 or is_premium_member 
+print(eligible_for_product_offer)
 
 
--- data_types_variables_exercises.py
+++ data_types_variables_exercises.py
@@ -36,5 +36,68 @@ type({'a': []})
 # A Matrix? - list of lists
 # The email addresses collected from a registration form? - list
 # Information about applicants to Codeup's data science program? - list of lists
-# 
+#
+'1' + 2
+# error
+
+6 % 4
+# 2
+type(6 % 4)
+# int
+type(type(6 % 4))
+# type
+
+'3 + 4 is ' + 3 + 4
+#type error because you can't add a string and ints like that
+
+0 < 0
+# False
+
+'False' == False
+# False
+True == 'True'
+# False
+
+5 >= -5
+# True
+
+True or "42"
+# True 
+# when using a bool with or, it returns the first True value
+# EX: print(list[]) or oopsie if the list is empty, thats false, so it will return the next thing 
+
+6 % 5
+# 1
+
+5 < 4 and 1 == 1
+# False
+'codeup' == 'codeup' and 'codeup' == 'Codeup'
+# False
+
+4 >= 0 and 1 !== '1'
+#invalid syntax: there's too many equal signs
+4 >= 0 and 1 != '1'
+# True
+
+6 % 3 == 0
+#True
+
+5 % 2 != 0
+#True 
+[1] + 2
+# Type error: can't join list to int
+[1] + [2]
+# [1, 2]
+[1] * 2
+#[1, 1] multiplies list by 2
+[1] * [2]
+# error, can't multiply a list by a list
+
+[] + [] == []
+#True
+
+{} + {}
+# you can't add dictionaries
+
+
 
