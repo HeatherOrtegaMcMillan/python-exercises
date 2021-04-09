fc946278dcfab78f28dc5baf75bb5b713608a516
Heather McMillan <heather.o.mcmillan@gmail.com>
2021-04-08 23:51:59 -0500

    upaload more exerciese done on the bonus python work

--- python_data_structure_manipulation_exercises.py
+++ python_data_structure_manipulation_exercises.py
@@ -131,20 +131,103 @@ num_of_light_coffee_drinkers
 
 # How many types of each pet are there?
 
+#iterate over list of students (dict), iterate over list of pets (dict or list of dict)
+#create new list of the different types of pets
+    #maybe create a list of all pets, then count the distinct ones
 
-# How many grades does each student have? Do they all have the same number of grades?
+
+pets_ppl_have = [] #initalize list of types of pets
+for p in students:   #loop through students
+    #print(p['pets'])
+    for kind in p['pets']: # loop through pets key 
+        # print(kind['species']) # print all pets in the species dicts
+        if kind['species'] not in pets_ppl_have: # check to see if pet name is unique
+            pets_ppl_have.append(kind['species']) #add it to list of pets
+print(pets_ppl_have)
+print("There are ", len(pets_ppl_have), " different types of pets")
+        
+# How many grades does each student have? 
+    # 4
+# Do they all have the same number of grades?
+    # yes they do
+
+x = 0 # initalize counter for student number for print chekc
+for grade in students: #iterate over list and find grades key
+    print("student", x , " has ", len(grade['grades']))  #count number of elements in that list
+    x += 1
+
 
 # What is each student's grade average?
+from statistics import mean
+
+for grade in students:
+    print(grade['student'], mean(grade["grades"]))
 
 # How many pets does each student have?
 
+for p in students:
+    print(p['student'], 'has', end = ' ')
+    number_pets = 0        #set counter to 0 for each person
+    for kind in p['pets']: #loop through pets dictionaries
+        # print(kind['species']) # print out the types of pets they have
+        number_pets += 1   #count up how many pets
+    print(number_pets, 'pet(s)') # print out the count of pets
+
 # How many students are in web development? data science?
+    # 7 students in web development and 7 in data science
+
+from collections import Counter
+# find list of all types of courses
+types_of_courses = [c['course'] for c in students] 
+# count distinct types in list 
+print(Counter(types_of_courses)) 
 
 # What is the average number of pets for students in web development?
 
+#find number of pets for each web development student
+# put values in list
+# average list
+from statistics import mean
+web_dev_pets = []
+for p in students:
+    if p['course'] != 'web development': #if student isn't in web dev, go to next student
+        continue
+    number_pets = 0        #set counter to 0 for each person
+    for kind in p['pets']: #loop through pets dictionaries
+        number_pets += 1   #count up how many pets
+    web_dev_pets.append(number_pets)
+print('the web development students have an average of ',
+      round(mean(web_dev_pets), 2), 'pets per student' #print satement and round the mean of list
+     )
+
 # What is the average pet age for students in data science?
+# find pets age for all data science students, make list
+# use mean to average that list together
+
+pet_ages = [] # initalize list to store ages
+for p in students:
+    if p['course'] != 'data science':
+        continue
+    for a in p['pets']:
+        pet_ages.append(a['age'])
+print("average pet age for data science students is ", 
+      round(mean(pet_ages), 2))
 
 # What is most frequent coffee preference for data science students?
+# find list of coffee preferences of ds students
+# do the counter thing again
+# there's probably a better way to do this
+
+coffee_prefs = [] # create list to store prefs
+for c in students:
+    if c['course'] != 'data science':
+        continue
+    else:
+        coffee_prefs.append(c['coffee_preference'])
+pref_counts = (Counter(coffee_prefs))
+print('most frequent coffee preference for data science students is\n', 
+      pref_counts.most_common(1)[0])
+
 
 # What is the least frequent coffee preference for web development students?
 
