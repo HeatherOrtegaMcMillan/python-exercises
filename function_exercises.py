b2ad3c4f7599cd129889c1e4a033915d47d47f5d
Heather McMillan <heather.o.mcmillan@gmail.com>
2021-04-12 15:37:41 -0500

    add lots of comments to function exercises

--- function_exercises.py
+++ function_exercises.py
@@ -2,12 +2,15 @@
 # 1. Define a function named is_two. 
 # It should accept one input and return True if the passed input is either:
 #  the number or the string 2, False otherwise.
-
+# the is_two function takes in a string or a number and returns True if the string or int is = 2
 def is_two(x):
+    # here we check to see if x is an int and == 2 and return True
     if x == 2:
         return True
+    # here we check to see if x is a string and == '2' True
     if x =='2':
         return True
+    # anything that does not meet the above conditions means the input was not 2, string or int
     else:
         return False
 
@@ -17,8 +20,16 @@ def is_two(x):
 # It should return True if the passed string is a vowel, False otherwise.
 
 def is_vowel(string):
+    """
+    takes in a string with a length of 1 (only one letter) outputs True if string is a vowel, False if it's not
+    """
+    # this is to check the type of the input to make sure it's a string
     if type(string) != str:
         return None
+    # if the length is more than 1 char long, will not work
+    if len(string) != 1:
+        return None
+    # checking the lowercased string to a list of vowels
     if string.lower() in ('a', 'e', 'i', 'o', 'u'):
         return True
     else:
@@ -31,10 +42,18 @@ print('2 is a vowel:', is_vowel(2), '  a is a vowel:', is_vowel('a'), '  t is a 
 # It should return True if the passed string is a consonant, False otherwise. 
 # Use your is_vowel function to accomplish this.
 
-def is_consonant(thing):
-    if type(thing) != str:
+def is_consonant(string):
+    """
+    takes in a string with a length of 1 (only one letter), outputs True if it's a consonant, False if it's not
+    """
+    # this is to check the type of the input to make sure it's a string
+    if type(string) != str:
         return None
-    if is_vowel(thing) == False:
+    # if the length is more than 1 char long, will not work
+    if len(string) != 1:
+        return None
+    # use previous function is_vowel to check reverse
+    if is_vowel(string) == False:
         return True
     else:
         return False
@@ -50,10 +69,18 @@ print('2 is a consonant:', is_consonant(2), '  t is a consonant:', is_consonant(
     # RETURN value is the string but capitalized 
 
 def capitalize_consonant(string):
+    """
+    Define a function that accepts a string that is a word 
+    The function capitalizes the first letter of the word if the word starts with a consonant
+
+    """
+    # checks to makesure input is string
     if type(string) != str: 
         return 'Type error: enter one word string'
+    # checks if first letter of string is a consonant then capatlizes word
     if is_consonant(string[0]) == True:
         return string.capitalize()
+    # if string doesn't start with consonant, original string is returned
     else:
         return string
 
@@ -69,8 +96,10 @@ def calculate_tip(tip_percent, bill_total):
     This function takes in the tip percentage (number between 0 and 1), and the total of the bill
     And returns the amount you should tip, rounded to two decimal places. 
     """
+    # check to make sure tip_percent is between 0 and 1, if outside of bounds function won't work
     if tip_percent < 0 or tip_percent > 1:
         return None
+    # return the calculated tip and round to nearest hundreth 
     else:
         return round((bill_total * tip_percent), 2)
 
@@ -85,8 +114,10 @@ def apply_discount(orig_price, discount_percent):
     This function takes in the original price, and the discount percentage (number between 0 and 1)
     And returns the price after the discount is applied.
     """
+    # check to make sure discount_percent is inbetween 0 and 1, if not function won't do anything for you
     if discount_percent < 0 or discount_percent > 1:
         return None
+    # return calculated percentage off added to the original price, rounded to nearest hundreth
     else:
         return round(((discount_percent * orig_price) + orig_price), 2)
 
@@ -104,8 +135,10 @@ def handle_commas(string):
     This function takes a string that is a number that contains commas in it as input 
     and return a number as output.
     """
-    if type(string) != str: # <---this check is weak it doesnt check to see if the input will actually work.
+    # check to make sure input is a string in order to continue
+    if type(string) != str: # <---this check is weak it doesnt check to see if the input has numbers in it
         return None
+    # remove any commas and cast input as an int
     return int(string.replace(',', ''))
 
 print('--------------- handle_commas check ------------')
@@ -115,6 +148,11 @@ print('Input: 12,345\nOutput:', handle_commas('12,345'))
 # It should accept a number and return the letter grade associated with that number (A-F).
 
 def get_letter_grade(grade):
+    """
+    This function takes a number as input (grade) 
+    and returns the letter grade associated with that number (A-F) as a string.
+    """
+    # below tests are to classify the inputed value (grade). if not in the 0-100 range, returns None
     if 88 <=  grade <= 100:
         return('A')
     elif 80 <= grade <= 87:
@@ -133,10 +171,18 @@ print(f'If your grade was 100, your letter grade is {get_letter_grade(100)}')
 # returns a string with all the vowels removed
 
 def remove_vowels(string):
+    """
+    This function takes a string and outputs a string with the vowels removed
+    """
+    # initalize a new string and set it to = the input string
     new_string = string
+    # loop through each letter in the string
     for letter in string:
+        # check to see if letter is a vowel (using previously defined function)
         if is_vowel(letter) == True:
+            #remove that letter
             new_string = new_string.replace(letter, '')
+    #all that should be left are the consonants 
     return new_string
 
 print('--------------- remove_vowels ------------')
@@ -153,16 +199,27 @@ print('Input: this is a test\n Output:', remove_vowels('this is a test'))
         # % Completed will become completed
 
 def normalize_name(string):
+    """
+    This function takes a string as input, and outputs a new string that is python friendly
+    it will remove whitespace and special charachters, lowercase the string, and replace the spaces with _
+    """
+    # take any whitespaces off of beginning or end
     string = string.strip()
+    # change to lowercase
     string = string.lower()
+    # replace spaces with _ (must be done before isalnum())
     string = string.replace(" ","_")
+    # go through each letter in string
     for letter in string:
+        # if letter is an alpha numeric charachter or an _ keep going
         if letter.isalnum() != False or letter == '_':
             continue
+        # anything else must be a special charachter so remove it 
         else:
             string = string.replace(letter, '')
+    # return 
     return string
-
+print('--------------- normalize_name check ------------')
 print('Input:   This is A %$Test  \nOutput:', normalize_name('   This is A %$Test  '))
 
 # 11. Write a function named cumulative_sum that accepts a list of numbers and returns a list that is the cumulative sum of the numbers in the list.
@@ -182,13 +239,21 @@ print('Input:   This is A %$Test  \nOutput:', normalize_name('   This is A %$Tes
 # new_list
 
 def cumulative_sum(list_of_numbers):
+    """
+    This function takes a list of numbers as input, and outputs a list of numbers 
+    where each one is added to the sum of the numbers before
+    """
+    # create empty list to store new values
     new_list = []
+    # add the first value from the inputted list to the new list
     new_list.append(list_of_numbers[0])
+    # use n to count from 1 to the length of the list  
     for n in range(1, len(list_of_numbers)):
+        #append the new list with the nth index of the list to the n-1th index of the new list
         new_list.append(list_of_numbers[n] + new_list[n-1])
     return new_list
 
-print('--------------- cumulative_sum ------------')
+print('--------------- cumulative_sum check ------------')
 print('Test List: [1, 2, 3]\nOutput:', cumulative_sum([1, 2, 3]))
 
 
